197bcf205eb35fa4b28b300e5ca8bb1629aa391a
KarthickrajaMuthuRaman <karthickraja.m@syncfusion.com>
2023-03-22 15:22:01 +0530

    documentation(EJ2-70836): To include UG for "Subtotals Position" and "Paging" for EJ2 Pivot Table server side engine

--- PivotController/PivotController/Controllers/PivotController.cs
+++ PivotController/PivotController/Controllers/PivotController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Newtonsoft.Json;
-using Syncfusion.Blazor.PivotView;
+using Syncfusion.EJ2.PivotView;
 
 namespace PivotController.Controllers
 {
