f5ac3518e3c46680a74aa9af3ad3fc063965f0cc
Karthickraja M <karthickraja.m@syncfusion.com>
2023-12-28 13:31:47 +0530

    862456: Need to update UG document for EJ2 platforms

--- PivotController/Controllers/PivotController.cs
+++ PivotController/Controllers/PivotController.cs
@@ -12,7 +12,7 @@ namespace PivotController.Controllers
         private readonly IMemoryCache _cache;
         private IWebHostEnvironment _hostingEnvironment;
         private bool isRendered;
-        private PivotEngine<PivotViewData> PivotEngine = new PivotEngine<PivotViewData>();
+        private PivotEngine<DataSource.PivotViewData> PivotEngine = new PivotEngine<DataSource.PivotViewData>();
         private ExcelExport excelExport = new ExcelExport();
 
         public PivotController(IMemoryCache cache, IWebHostEnvironment environment)
@@ -85,7 +85,15 @@ namespace PivotController.Controllers
 
                     // Here, you can refer different kinds of data sources. We've bound a collection in this illustration.
                     // return new PivotViewData().GetVirtualData();
-                    return new PivotViewData().GetVirtualData();
+                    return new DataSource.PivotViewData().GetVirtualData();
+
+                    // EXAMPLE:
+                    // Other data sources, such as DataTable, CSV, JSON, etc., can be bound as shown below.
+                    // return new DataSource.BusinessObjectsDataView().GetDataTable();
+                    // return new DataSource.PivotJSONData().ReadJSONData(_hostingEnvironment.ContentRootPath + "\\DataSource\\sales-analysis.json");
+                    // return new DataSource.PivotCSVData().ReadCSVData(_hostingEnvironment.ContentRootPath + "\\DataSource\\sales.csv");
+                    // return new DataSource.PivotJSONData().ReadJSONData("http://cdn.syncfusion.com/data/sales-analysis.json");
+                    // return new DataSource.PivotCSVData().ReadCSVData("http://cdn.syncfusion.com/data/sales-analysis.csv");
                 });
 #pragma warning restore CS8603 // Possible null reference return.
         }
@@ -116,6 +124,10 @@ namespace PivotController.Controllers
         private async Task<object> GetPivotValues(FetchData param)
         {
             EngineProperties engine = await GetEngine(param);
+            if (param.IsGroupingUpdated)
+            {
+                engine.Data = await GetData(param);
+            }
             if (!isRendered)
             {
                 engine = await PivotEngine.PerformAction(engine, param);
@@ -123,36 +135,6 @@ namespace PivotController.Controllers
             _cache.Remove("engine" + param.Hash);
             _cache.Set("engine" + param.Hash, engine, new MemoryCacheEntryOptions().SetSize(1).SetAbsoluteExpiration(DateTimeOffset.UtcNow.AddMinutes(60)));
             return PivotEngine.GetSerializedPivotValues();
-        }
-
-    }
-    public class PivotViewData
-    {
-        public string ProductID { get; set; }
-        public string Country { get; set; }
-        public string Product { get; set; }
-        public double Sold { get; set; }
-        public double Price { get; set; }
-        public string Year { get; set; }
-
-        public List<PivotViewData> GetVirtualData()
-        {
-            List<PivotViewData> VirtualData = new List<PivotViewData>();
-
-            for (int i = 1; i <= 100; i++)
-            {
-                PivotViewData p = new PivotViewData
-                {
-                    ProductID = "PRO-" + (100 + i),
-                    Year = (new string[] { "FY 2015", "FY 2016", "FY 2017", "FY 2018", "FY 2019" })[new Random().Next(5)],
-                    Country = (new string[] { "Canada", "France", "Australia", "Germany", "France" })[new Random().Next(5)],
-                    Product = (new string[] { "Car", "Van", "Bike", "Flight", "Bus" })[new Random().Next(5)],
-                    Price = (3.4 * i) + 500,
-                    Sold = (i * 15) + 10
-                };
-                VirtualData.Add(p);
-            }
-            return VirtualData;
         }
     }
 }
